8058b0a9b5cb16aca518475444de2ca48b465032
Izhar <izharramdan93@gmail.com>
2024-07-03 20:54:14 +0700

    update

--- todo_app/script.js
+++ todo_app/script.js
@@ -24,6 +24,8 @@ const addOrUpdateTask = () => {
 
     if (dataArrIndex === -1) {
         taskData.unshift(taskObj);
+    } else {
+        taskData[dataArrIndex] = taskObj;
     }
 
     updateTaskContainer()
@@ -48,11 +50,30 @@ const updateTaskContainer = () => {
     );
 };
 
+
 const deleteTask = (buttonEl) => {
     const dataArrIndex = taskData.findIndex(
         (item) => item.id === buttonEl.parentElement.id
     );
 
+    buttonEl.parentElement.remove();
+    taskData.splice(dataArrIndex, 1);
+}
+
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+        (item) => item.id === buttonEl.parentElement.id
+    );
+
+    currentTask = taskData[dataArrIndex];
+
+    titleInput.value = currentTask.title;
+    dateInput.value = currentTask.date;
+    descriptionInput.value = currentTask.description;
+
+    addOrUpdateTaskBtn.innerText = "Update Task";
+
+    taskForm.classList.toggle("hidden");
 }
 
 const reset = () => {
@@ -69,7 +90,9 @@ openTaskFormBtn.addEventListener("click", () =>
 
 closeTaskFormBtn.addEventListener("click", () => {
     const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
-    if (formInputsContainValues) {
+    const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
+
+    if (formInputsContainValues && formInputValuesUpdated) {
         confirmCloseDialog.showModal();
     } else {
         reset();
@@ -88,3 +111,11 @@ taskForm.addEventListener("submit", (e) => {
 
     addOrUpdateTask();
 });
+
+const myTaskArr = [
+    { task: "Walk the Dog", date: "22-04-2022" },
+    { task: "Read some books", date: "02-11-2023" },
+    { task: "Watch football", date: "10-08-2021" },
+];
+
+localStorage.setItem("data", myTaskArr);
