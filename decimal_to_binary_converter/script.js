57aa8c5c228a8aa0c54d0491758b2c307afa31ec
Izhar <izharramdan93@gmail.com>
2024-07-04 19:09:39 +0700

    recursive

--- decimal_to_binary_converter/script.js
+++ decimal_to_binary_converter/script.js
@@ -1,42 +1,52 @@
-
-
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const countdown = (number) => {
+  console.log(number);
+
+  if (number === 0) {
+    return;
+  } else {
+    countdown(number - 1);
+  }
+};
+
+
+
 const decimalToBinary = (input) => {
-    let binary = "";
+  let binary = "";
 
-    if (input === 0) {
-        binary = "0";
-    }
+  if (input === 0) {
+    binary = "0";
+  }
 
-    while (input > 0) {
-        binary = (input % 2) + binary;
-        input = Math.floor(input / 2);
-    }
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+  }
 
-    result.innerText = binary;
+  result.innerText = binary;
 };
 
 const checkUserInput = () => {
-    if (
-        !numberInput.value ||
-        isNaN(parseInt(numberInput.value)) ||
-        parseInt(numberInput.value) < 0
-    ) {
-        alert("Please provide a decimal number greater than or equal to 0");
-        return;
-    }
+  if (
+    !numberInput.value ||
+    isNaN(parseInt(numberInput.value)) ||
+    parseInt(numberInput.value) < 0
+  ) {
+    alert("Please provide a decimal number greater than or equal to 0");
+    return;
+  }
 
-    decimalToBinary(parseInt(numberInput.value));
-    numberInput.value = "";
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
 };
 
 convertBtn.addEventListener("click", checkUserInput);
 
 numberInput.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-        checkUserInput();
-    }
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
 });
