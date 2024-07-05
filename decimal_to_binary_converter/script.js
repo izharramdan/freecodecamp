5aab6cc96f19c6d769e1601139bfa53a6930059d
Izhar <izharramdan93@gmail.com>
2024-07-05 13:23:46 +0700

    update recursion

--- decimal_to_binary_converter/script.js
+++ decimal_to_binary_converter/script.js
@@ -2,31 +2,12 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const countdown = (number) => {
-  console.log(number);
-
-  if (number === 0) {
-    return;
-  } else {
-    countdown(number - 1);
-  }
-};
-
-
-
 const decimalToBinary = (input) => {
-  let binary = "";
-
   if (input === 0) {
-    binary = "0";
+    return "";
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-
-  while (input > 0) {
-    binary = (input % 2) + binary;
-    input = Math.floor(input / 2);
-  }
-
-  result.innerText = binary;
 };
 
 const checkUserInput = () => {
@@ -39,7 +20,7 @@ const checkUserInput = () => {
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
 };
 
