eafc7805289abcf638516b43e6d2992a5129c1de
Izhar <izharramdan93@gmail.com>
2024-07-12 14:04:15 +0700

    update

--- number_sorter/script.js
+++ number_sorter/script.js
@@ -1,10 +1,22 @@
 const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
-  event.preventDefault();
-  const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => dropdown.value);
-  console.log(inputValues);
+    event.preventDefault();
+
+    const inputValues = [
+        ...document.getElementsByClassName("values-dropdown")
+    ].map((dropdown) => Number(dropdown.value));
+
+    updateUI(inputValues);
 }
+
+const updateUI = (array = []) => {
+    array.forEach((num, i) => {
+        const outputValueNode = document.getElementById(`output-value-${i}`);
+        outputValueNode.innerText = num;
+    })
+}
+
 
 
 sortButton.addEventListener("click", sortInputArray);
