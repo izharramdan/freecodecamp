eace99805c5422e8837825e4c1861de00604cc9c
Izhar <izharramdan93@gmail.com>
2024-07-14 11:11:18 +0700

    range and variance

--- statistic_calculator/script.js
+++ statistic_calculator/script.js
@@ -9,13 +9,38 @@ const getMedian = (array) => {
   return median;
 }
 
-
 const getMode = (array) => {
   const counts = {};
-  array.forEach(el => counts[el] = (counts[el] || 0) + 1)
-  console.log(counts)
-  return counts;
+  array.forEach((el) => {
+    counts[el] = (counts[el] || 0) + 1;
+  })
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
+  }
+  const highest = Object.keys(counts).sort(
+    (a, b) => counts[b] - counts[a]
+  )[0];
+  const mode = Object.keys(counts).filter(
+    (el) => counts[el] === counts[highest]
+  );
+  return mode.join(", ");
 }
+
+const getRange = (array) => {
+  return Math.max(...array) - Math.min(...array);
+}
+
+const getVariance = (array) => {
+  const mean = getMean(array);
+  const variance = array.reduce((acc, el) => {
+    const difference = el - mean;
+    const squared = difference ** 2;
+    return acc + squared;
+  }, 0) / array.length;
+  return variance;
+}
+
+
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -24,8 +49,13 @@ const calculate = () => {
   
   const mean = getMean(numbers);
   const median = getMedian(numbers);
-  console.log(getMode(numbers));
+  const mode = getMode(numbers);
+  const range = getRange(numbers);
+  const variance = getVariance(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
+  document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
 }
