0290b6666272b902c5b3db76b0072772a64fdbb7
Izhar <izharramdan93@gmail.com>
2024-07-28 05:50:01 +0700

    update

--- learn_basic_OOP_by_buildiing_shopping_cart/script.js
+++ learn_basic_OOP_by_buildiing_shopping_cart/script.js
@@ -1,3 +1,3 @@
 const cartContainer = document.getElementById("cart-container");
 const productsContainer = document.getElementById("products-container");
-const dessertCards = document.getElementById("dessert-container");
+const dessertCards = document.getElementById("dessert-card-container");
