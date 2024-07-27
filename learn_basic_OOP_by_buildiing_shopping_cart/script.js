76e9da27b93ee8f2fc2d7390251126b89d9b6117
Izhar <izharramdan93@gmail.com>
2024-07-28 06:12:32 +0700

    update

--- learn_basic_OOP_by_buildiing_shopping_cart/script.js
+++ learn_basic_OOP_by_buildiing_shopping_cart/script.js
@@ -7,3 +7,15 @@ const totalNumberOfItems = document.getElementById("total-items");
 const cartSubTotal = document.getElementById("subtotal");
 const cartTaxes = document.getElementById("taxes");
 const cartTotal = document.getElementById("total");
+const showHideCartSpan = document.getElementById("show-hide-cart");
+let isCartShowing = false;
+
+const products = [
+    {
+        id: 1,
+        name: "Vanilla Cupcakes (6 Pack)",
+        price: 12.99,
+        category: "Cupcake",
+    },
+
+];
