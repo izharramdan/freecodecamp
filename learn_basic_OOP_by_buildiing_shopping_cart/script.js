62bcb32dde9eba8a5e4b6f1f3776aab78f00566f
Izhar <izharramdan93@gmail.com>
2024-07-29 05:38:43 +0700

    update

--- learn_basic_OOP_by_buildiing_shopping_cart/script.js
+++ learn_basic_OOP_by_buildiing_shopping_cart/script.js
@@ -11,83 +11,83 @@ const showHideCartSpan = document.getElementById("show-hide-cart");
 let isCartShowing = false;
 
 const products = [
-    {
-        id: 1,
-        name: "Vanilla Cupcakes (6 Pack)",
-        price: 12.99,
-        category: "Cupcake",
-    },
-    {
-        id: 2,
-        name: "French Macaron",
-        price: 3.99,
-        category: "Macaron",
-    },
-    {
-        id: 3,
-        name: "Pumpkin Cupcake",
-        price: 3.99,
-        category: "Cupcake",
-    },
-    {
-        id: 4,
-        name: "Chocolate Cupcake",
-        price: 5.99,
-        category: "Cupcake",
-    },
-    {
-        id: 5,
-        name: "Chocolate Pretzels (4 Pack)",
-        price: 10.99,
-        category: "Pretzel",
-    },
-    {
-        id: 6,
-        name: "Strawberry Ice Cream",
-        price: 2.99,
-        category: "Ice Cream",
-    },
-    {
-        id: 7,
-        name: "Chocolate Macarons (4 Pack)",
-        price: 9.99,
-        category: "Macaron",
-    },
-    {
-        id: 8,
-        name: "Strawberry Pretzel",
-        price: 4.99,
-        category: "Pretzel",
-    },
-    {
-        id: 9,
-        name: "Butter Pecan Ice Cream",
-        price: 2.99,
-        category: "Ice Cream",
-    },
-    {
-        id: 10,
-        name: "Rocky Road Ice Cream",
-        price: 2.99,
-        category: "Ice Cream",
-    },
-    {
-        id: 11,
-        name: "Vanilla Macarons (5 Pack)",
-        price: 11.99,
-        category: "Macaron",
-    },
-    {
-        id: 12,
-        name: "Lemon Cupcakes (4 Pack)",
-        price: 12.99,
-        category: "Cupcake",
-    },
+  {
+    id: 1,
+    name: "Vanilla Cupcakes (6 Pack)",
+    price: 12.99,
+    category: "Cupcake",
+  },
+  {
+    id: 2,
+    name: "French Macaron",
+    price: 3.99,
+    category: "Macaron",
+  },
+  {
+    id: 3,
+    name: "Pumpkin Cupcake",
+    price: 3.99,
+    category: "Cupcake",
+  },
+  {
+    id: 4,
+    name: "Chocolate Cupcake",
+    price: 5.99,
+    category: "Cupcake",
+  },
+  {
+    id: 5,
+    name: "Chocolate Pretzels (4 Pack)",
+    price: 10.99,
+    category: "Pretzel",
+  },
+  {
+    id: 6,
+    name: "Strawberry Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+  },
+  {
+    id: 7,
+    name: "Chocolate Macarons (4 Pack)",
+    price: 9.99,
+    category: "Macaron",
+  },
+  {
+    id: 8,
+    name: "Strawberry Pretzel",
+    price: 4.99,
+    category: "Pretzel",
+  },
+  {
+    id: 9,
+    name: "Butter Pecan Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+  },
+  {
+    id: 10,
+    name: "Rocky Road Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+  },
+  {
+    id: 11,
+    name: "Vanilla Macarons (5 Pack)",
+    price: 11.99,
+    category: "Macaron",
+  },
+  {
+    id: 12,
+    name: "Lemon Cupcakes (4 Pack)",
+    price: 12.99,
+    category: "Cupcake",
+  },
 ];
 
 products.forEach(
-    ({ name, id, price, category }) => {
-        dessertCards.innerHTML += `
+  ({ name, id, price, category }) => {
+    dessertCards.innerHTML += `
       <div class="dessert-card">
         <h2>${name}</h2>
         <p class="dessert-price">$${price}</p>
@@ -98,15 +98,21 @@ products.forEach(
         </button>
       </div>
     `;
-    }
+  }
 );
 
 class ShoppingCart {
-    constructor() {
-        this.items = [];
-        this.total = 0;
-        this.taxRate = 8.25;
-    }
+  constructor() {
+    this.items = [];
+    this.total = 0;
+    this.taxRate = 8.25;
+  }
+
+  addItem(id, products) {
+    const product = products.find((item) => item.id === id);
+    const { name, price } = product;
+    this.items.push(product);
 
 
+  }
 };
