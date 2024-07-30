980327975ed8ddcfaa7c5aeaafa4facd78bec645
Izhar <izharramdan93@gmail.com>
2024-07-31 05:36:44 +0700

    update script

--- learn_basic_OOP_by_buildiing_shopping_cart/script.js
+++ learn_basic_OOP_by_buildiing_shopping_cart/script.js
@@ -144,6 +144,10 @@ class ShoppingCart {
   calculateTotal() {
     const subTotal = this.items.reduce((total, item) => total + item.price, 0);
     const tax = this.calculateTaxes(subTotal);
+    this.total = subTotal + tax;
+    cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+    cartTaxes.textContent = `$${tax.toFixed(2)}`;
+    cartTotal.textContent = `$${this.total.toFixed(2)}`;
 
   }
 };
