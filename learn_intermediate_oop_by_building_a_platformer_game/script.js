ed5268c52bfb168a2c2b669291129689f6caed57
Izhar <izharramdan93@gmail.com>
2024-08-11 06:55:27 +0700

    update

--- learn_intermediate_oop_by_building_a_platformer_game/script.js
+++ learn_intermediate_oop_by_building_a_platformer_game/script.js
@@ -71,6 +71,26 @@ class Platform {
   }
 }
 
+
+class CheckPoint {
+  constructor(x, y, z) {
+    this.position = {
+      x,
+      y,
+    };
+    this.width = proportionalSize(40);
+    this.height = proportionalSize(70);
+    this.claimed = false;
+  };
+
+  draw() {
+    ctx.fillStyle = "#f1be32";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+};
+
+
 const player = new Player();
 
 const platformPositions = [
@@ -142,14 +162,11 @@ const animate = () => {
       player.position.y <= platform.position.y + platform.height,
     ];
 
-
     if (platformDetectionRules.every(rule => rule)) {
       player.position.y = platform.position.y + player.height;
       player.velocity.y = gravity;
     };
-
   });
-
 }
 
 
