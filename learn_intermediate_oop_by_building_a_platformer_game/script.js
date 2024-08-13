6698790973e3b8f8a813eab366731f82a279ac3e
Izhar <izharramdan93@gmail.com>
2024-08-14 06:03:30 +0700

    update

--- learn_intermediate_oop_by_building_a_platformer_game/script.js
+++ learn_intermediate_oop_by_building_a_platformer_game/script.js
@@ -202,12 +202,14 @@ const animate = () => {
       player.position.y + player.height <=
         checkpoint.position.y + checkpoint.height,
       isCheckpointCollisionDetectionActive,
-
+      player.position.x - player.width <=
+        checkpoint.position.x - checkpoint.width + player.width * 0.9,
+      index === 0 || checkpoints[index - 1].claimed === true,
     ];
+
   });
 
 }
-
 
 const keys = {
   rightKey: {
