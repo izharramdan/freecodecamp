3f9ef01c92a70739efc9ffcbaa2f68704085945e
Izhar <izharramdan93@gmail.com>
2024-09-20 20:32:10 +0700

    update

--- dice_game/script.js
+++ dice_game/script.js
@@ -13,34 +13,108 @@ const rulesBtn = document.getElementById("rules-btn");
 let diceValuesArr = [];
 let isModalShowing = false;
 let score = 0;
-let round = 1;
-let rolls = 0;
-
-rulesBtn.addEventListener("click", () => {
-    isModalShowing = !isModalShowing;
-    if (isModalShowing) {
-        rulesContainer.style.display = "block";
-        rulesBtn.textContent = "Hide rules";
-    } else {
-        rulesContainer.style.display = "none";
-        rulesBtn.textContent = "Show rules";
-
-    }
-});
+let round = 1; 
+let rolls = 0; 
 
 const rollDice = () => {
-    diceValuesArr = [];
+  diceValuesArr = [];
 
-    for (let i = 0; i < 5; i++) {
-        const randomDice = Math.floor(Math.random() * 6) + 1;
-        diceValuesArr.push(randomDice);
-    };
+  for (let i = 0; i < 5; i++) {
+    const randomDice = Math.floor(Math.random() * 6) + 1;
+    diceValuesArr.push(randomDice);
+  };
 
-    listOfAllDice.forEach((dice, index) => {
-        dice.textContent = diceValuesArr[index];
-    });
+  listOfAllDice.forEach((dice, index) => {
+    dice.textContent = diceValuesArr[index];
+  });
+};
+
+const updateStats = () => {
+  rollsElement.textContent = rolls;
+  roundElement.textContent = round;
+};
+
+const updateRadioOption = (index, score) => {
+  scoreInputs[index].disabled = false;
+  scoreInputs[index].value = score;
+  scoreSpans[index].textContent = `, score = ${score}`;
+};
+
+const updateScore = (selectedValue, achieved) => {
+  score += parseInt(selectedValue);
+  totalScoreElement.textContent = score;
+
+  scoreHistory.innerHTML += `<li>${achieved} : ${selectedValue}</li>`;
+};
+
+const getHighestDuplicates = (arr) => {
+  const counts = {};
+
+  for (const num of arr) {
+    if (counts[num]) {
+      counts[num]++;
+    } else {
+      counts[num] = 1;
+    }
+  }
+
+  let highestCount = 0;
+
+  for (const num of arr) {
+    const count = counts[num];
+    if (count >= 3 && count > highestCount) {
+      highestCount = count;
+    }
+    if (count >= 4 && count > highestCount) {
+      highestCount = count;
+    }
+  }
+
+  const sumOfAllDice = arr.reduce((a, b) => a + b, 0);
+
+  if (highestCount >= 4) {
+    updateRadioOption(1, sumOfAllDice);
+  }
+
+  if (highestCount >= 3) {
+    updateRadioOption(0, sumOfAllDice);
+  }
+
+  updateRadioOption(5, 0);
+};
+
+const resetRadioOptions = () => {
+  scoreInputs.forEach((input) => {
+    input.disabled = true;
+    input.checked = false;
+  });
+
+  scoreSpans.forEach((span) => {
+    span.textContent = "";
+  });
 };
 
 rollDiceBtn.addEventListener("click", () => {
+  if (rolls === 3) {
+    alert("You have made three rolls this round. Please select a score.");
+  } else {
+    rolls++;
+    resetRadioOptions();
     rollDice();
+    updateStats();
+    getHighestDuplicates(diceValuesArr);
+  }
 });
+
+rulesBtn.addEventListener("click", () => {
+  isModalShowing = !isModalShowing;
+
+  if (isModalShowing) {
+    rulesBtn.textContent = "Hide rules";
+    rulesContainer.style.display = "block";
+  } else {
+    rulesBtn.textContent = "Show rules";
+    rulesContainer.style.display = "none";
+  }
+});
+
